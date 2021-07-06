bc9c7b07f4f5cd66cf43dd99549e3d23e95218e5
JoaoPorto23 <rm87082@fiap.com.br>
2021-07-06 19:24:18 -0300

    Inseri imagens / finalização do Menu

---
+++ js/script.js
@@ -0,0 +1,51 @@
+var coracao2=document.querySelector("#heart2");
+
+function heartClick(){
+var coracao=document.querySelector("#heart");
+    coracao.classList.toggle("red-heart")  
+}
+function heartClick2(){
+        coracao2.classList.toggle("red-heart")  
+}
+function heartClick3(){
+    var coracao3=document.querySelector("#heart3");
+        coracao3.classList.toggle("red-heart")  
+}
+function heartClick4(){
+    var coracao4=document.querySelector("#heart4");
+        coracao4.classList.toggle("red-heart")  
+}
+function heartClick5(){
+    var coracao5=document.querySelector("#heart5");
+        coracao5.classList.toggle("red-heart")  
+}
+function heartClick6(){
+    var coracao6=document.querySelector("#heart6");
+        coracao6.classList.toggle("red-heart")  
+}
+function heartClick7(){
+    var coracao7=document.querySelector("#heart7");
+        coracao7.classList.toggle("red-heart")  
+}
+function heartClick8(){
+    var coracao8=document.querySelector("#heart8");
+        coracao8.classList.toggle("red-heart")  
+}
+function heartClick9(){
+    var coracao9=document.querySelector("#heart9");
+        coracao9.classList.toggle("red-heart")  
+}
+
+function heartClick10(){
+    var coracao10=document.querySelector("#heart10");
+        coracao10.classList.toggle("red-heart")  
+}
+function heartClick11(){
+    var coracao11=document.querySelector("#heart11");
+        coracao11.classList.toggle("red-heart")  
+}
+function heartClick12(){
+    var coracao12=document.querySelector("#heart12");
+        coracao12.classList.toggle("red-heart")  
+}
+
